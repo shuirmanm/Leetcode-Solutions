db305e92c03a4eb5a9c8805cbbfc0124f3f80312
shuirmanm <87150214+shuirmanm@users.noreply.github.com>
2022-08-14 11:26:17 -0500

    Changed loop so the answer could not read 'True' if it fails at any point

--- Leetcode Easy/palindrome_number.py
+++ Leetcode Easy/palindrome_number.py
@@ -11,10 +11,13 @@ For example, 121 is a palindrome while 123 is not.
 x = 121
 
 #Solution
-converted_x = str(x)
-answer = False
-for i in range(len(converted_x)-1):
-  if converted_x[i] == converted_x[len(converted_x)-1-i]:
+    converted_x = str(x)
     answer = True
-  else:
-    answer = False
+    for i in range(len(converted_x)-1):
+      if converted_x[i] == converted_x[len(converted_x)-1-i]:
+        if answer == False:
+          pass
+        else:
+          answer = True
+      else:
+        answer = False
