8eb04721b9621a63799f68ef0f075fb5409e8eea
shuirmanm <87150214+shuirmanm@users.noreply.github.com>
2022-08-14 11:18:44 -0500

    Create palindrome_number.py

---
+++ Leetcode Easy/palindrome_number.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+"""
+
+#Inputs
+x = 121
+
+#Solution
+converted_x = str(x)
+answer = False
+for i in range(len(converted_x)-1):
+  if converted_x[i] == converted_x[len(converted_x)-1-i]:
+    answer = True
+  else:
+    answer = False
